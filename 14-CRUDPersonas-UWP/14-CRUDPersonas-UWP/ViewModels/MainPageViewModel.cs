b3ab8407ba0f5f87172355e8173f57854ca35106
lz <luiszumarraga95@gmail.com>
2018-11-08 14:43:02 +0100

    commit

---
+++ 14-CRUDPersonas-UWP/14-CRUDPersonas-UWP/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,72 @@
+﻿using _14_CRUPPersonas_Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_CRUDPersonas_UWP.ViewModels {
+    public class MainPageViewModel : INotifyPropertyChanged {
+
+        #region propiedades privadas
+
+        private List<clsPersona> _ListadoDePersonas;
+        private clsPersona _PersonaSeleccionada;
+        private List<clsDepartamento> _ListadoDepartamentos;
+
+
+
+        #endregion
+
+        #region propiedades publicas
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<clsPersona> ListadoDePersonas {
+            get {
+                return _ListadoDePersonas;
+            }
+            set {
+                _ListadoDePersonas = value;
+            }
+        }
+
+        public clsPersona PersonaSeleccionada {
+            get {
+                return _PersonaSeleccionada;
+            }
+            set /*fernando apruebame*/{
+                _PersonaSeleccionada = value;
+                OnPropertyChanged("PersonaSeleccionada");
+            }
+        }
+
+        public List<clsDepartamento> ListadoDepartamentos {
+            get {
+                return _ListadoDepartamentos;
+            }
+            set {
+                _ListadoDepartamentos = value;
+            }
+        }
+
+        #endregion
+
+        #region constructores
+
+        public MainPageViewModel() {
+            //Cargar listado de personas
+            _ListadoDePersonas = clsListadoPersonas.listadoPersonas();
+        }
+        #endregion
+
+        #region otros
+        protected void OnPropertyChanged(string name) {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+    }
+}
---
+++ 14-CRUDPersonas-UWP/14-CRUPPersonas-Entities/clsListadoPersonas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_CRUPPersonas_Entities {
+    public class clsListadoPersonas
+    {
+
+        /// <summary>
+        /// Metodo que devolvera una lista de objetos persona con datos predefinidos
+        /// </summary>
+        /// <returns></returns>
+        public static List<clsPersona> listadoPersonas() {
+
+             List<clsPersona> listado = new List<clsPersona>();
+
+            listado.Add(new clsPersona(1, "Jorge", "Obando Lopez", new DateTime(1998, 10, 9), "Calle patata", "1111111"));
+            listado.Add(new clsPersona(2, "Gorge", "Obandino Lopecio", new DateTime(1998, 10, 9), "Calle botella", "1111111"));
+            listado.Add(new clsPersona(3, "Jorgue", "Hovando Pezlo", new DateTime(1998, 10, 9), "Calle alberto", "1111111"));
+            listado.Add(new clsPersona(3, "Juan", "Moreno Poza", new DateTime(1998, 10, 9), "Calle falsa", "1111111"));
+            listado.Add(new clsPersona(3, "Alberto", "Rezno Ploter", new DateTime(1998, 10, 9), "Calle almibar", "1111111"));
+            listado.Add(new clsPersona(3, "Quisquilla", "Pelusa Po", new DateTime(1998, 10, 9), "Calle estiercol", "1111111"));
+
+         
+
+            return listado;
+
+
+        }
+
+    }
+}
