df61c8b176a5c070de3a0e22b2ed791056b2be68
lz <luiszumarraga95@gmail.com>
2018-11-26 14:38:46 +0100

    Alfredi

---
+++ 14-CRUDPersonas-UWP/14-CRUDPersonas-UWP/ViewModels/Converters/clsConversorFechaCorta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace _14_CRUDPersonas_UWP.ViewModels.Converters {
+    class clsConversorFechaCorta : IValueConverter{
+        public object Convert(object value, Type targetType, object parameter, string language) {
+
+            DateTime fecha = (DateTime)value;
+
+            return fecha.ToShortDateString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+
+            DateTime resultado;
+
+            DateTime.TryParse((String)value, out resultado);
+
+            return resultado;
+        }
+    }
+}
---
+++ 14-CRUDPersonas-UWP/14-CRUDPersonas-UWP/ViewModels/Converters/clsDateTimeToDateTimeOffset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace _14_CRUDPersonas_UWP.ViewModels.Converters {
+    class clsDateTimeToDateTimeOffset : IValueConverter{
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            DateTime fecha = (DateTime)value;
+            return new DateTimeOffset(fecha);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            DateTimeOffset resultado;
+            return resultado.DateTime;
+        }
+    }
+}
